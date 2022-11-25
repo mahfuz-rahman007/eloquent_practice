77f4d459496eb70e39eb8987e85447b924dd90d3
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2022-11-25 23:22:00 +0600

    laravel updated to 9

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,7 +32,7 @@ class HomeController extends Controller
                         ])
                         ->take(1)->get();
 
-
+    $customers = Customer::all();
 
         return view('home', compact('movies','customers'));
     }
